68e78ef9218fa55fdb6390f229c6f4bbb05de359
Stephen <stephen.r.avila@gmail.com>
2020-03-21 13:35:15 -0400

    conditionals

---
+++ script.js
@@ -0,0 +1,33 @@
+// var likesDogs = true;
+
+// if (likesDogs === true) {
+//    console.log("You're a dog person!") 
+// } else if (likesDogs === false) {
+//    console.log("You're a monster")
+// }
+
+// var birthYear = 1989;
+
+// if (birthYear === 1989) {
+//     console.log("You're an Echo Boomer")
+// } else if (birthYear === 1960) {
+//     console.log("Regular Boomer")
+// } else if (birthYear === 2010) {
+//     console.log("New Boomer")
+// }
+    
+// var stephenObject = {
+//     firstName: "Stephen",
+//     prefLanguage: "English",
+// }
+// console.log(stephenObject.firstName);
+// console.log(stephenObject.prefLanguage);
+
+// var firstName = "Stephen";
+// var prefLanguage = "English";
+
+// if (firstName === prefLanguage) {
+//     console.log("Hello Stephen!")
+// } else {
+//     console.log("Hola Stephen!")
+// }
